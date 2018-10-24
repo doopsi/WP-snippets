820c6a4ee89e546aeda1e281f0d03e8dce8e6ec8
Cecilia Alessandrini <cecilia.alessandrini@gmail.com>
2018-10-24 16:20:21 +0200

    Update ACF Repeater in flexible content.php

--- ACF Repeater in flexible content.php
+++ ACF Repeater in flexible content.php
@@ -1,24 +1,16 @@
 <?php
 // check if the flexible content field has rows of data
 if( have_rows('contenuto') ):
-
      // loop through the rows of data
     while ( have_rows('contenuto') ) : the_row();
-
         if( get_row_layout() == 'testo' ):
-
             $content = get_sub_field('blocco_di_testo');
-
             echo '<div class="text-block"> ' . $content . ' </div>';
-
             elseif( get_row_layout() == 'video' ): 
-
                 $video = get_sub_field('blocco_video');
                 
                 echo '<div class="embed-video"> '. $video . ' </div>';
-
             elseif( get_row_layout() == 'slider' ):
-
                 echo '<div class="bxslider">';
                 
                 // check if the nested repeater field has rows of data
@@ -35,13 +27,10 @@ if( have_rows('contenuto') ):
                     endwhile;
                     
                     echo '</div>';
-
                 endif;
         endif;
     endwhile;
 else :
-
     // no layouts found
-
 endif;
 ?>
