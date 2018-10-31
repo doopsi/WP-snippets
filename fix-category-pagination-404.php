0bcc8c04883e9893399bed3654936a8300878d01
Cecilia Alessandrini <cecilia.alessandrini@gmail.com>
2018-10-31 06:08:29 +0100

    Create fix-category-pagination-404.php

---
+++ fix-category-pagination-404.php
@@ -0,0 +1,11 @@
+function fix_category_pagination($qs){
+	if(isset($qs['category_name']) && isset($qs['paged'])){
+		$qs['post_type'] = get_post_types($args = array(
+			'public'   => true,
+			'_builtin' => false
+		));
+		array_push($qs['post_type'],'post');
+	}
+	return $qs;
+}
+add_filter('request', 'fix_category_pagination');
